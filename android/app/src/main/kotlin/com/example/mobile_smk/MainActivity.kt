0bf9e470503268b84c49d3a714f27eed1b6ac16d
candra06 <candrabiyu4@gmail.com>
2021-11-19 16:01:36 +0700

    Pertama Kali Upload

---
+++ android/app/src/main/kotlin/com/example/mobile_smk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_smk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget helloWorld() {
+    return Text(1.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home Page'),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.black,
+                margin: EdgeInsets.all(16),
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  'Home Page',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                margin: EdgeInsets.all(16),
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  'Home Page',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                margin: EdgeInsets.all(16),
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  'Home Page',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
